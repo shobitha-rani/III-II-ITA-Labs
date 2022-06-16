dac7649f74bba886358d9f3f4ec73ecfe46bfeb2
20wh5a1205 SHOBITHARANI <81136519+shobitha-rani@users.noreply.github.com>
2022-06-16 21:57:24 +0530

    Update .java

--- MAD/files/.java
+++ MAD/files/.java
@@ -1,1 +1,73 @@
+package com.example.files;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+public class MainActivity extends AppCompatActivity {
+    public static String file="\\home\\oem\\Documents\\sample.txt";
+    EditText uname,password;
+    Button save,login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        uname=(EditText)findViewById(R.id.editText1);
+        password=(EditText)findViewById(R.id.editText2);
+        save=(Button)findViewById(R.id.button);
+        login=(Button)findViewById(R.id.button2);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    FileOutputStream fout = openFileOutput(file,MODE_PRIVATE);
+                    String data = "user1 123\nuser2 234\nuser3 345";
+                    fout.write(data.getBytes());
+                    fout.close();
+                    Toast.makeText(getBaseContext(),"details saved"+getFileStreamPath(file),Toast.LENGTH_SHORT).show();
+                }catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uname_ = uname.getText().toString();
+                String password_=password.getText().toString();
+                try{
+                    FileInputStream fin = openFileInput(file);
+                    int c;
+                    String temp="";
+                    while((c=fin.read())!=-1){
+                        temp=temp+Character.toString((char)c);
+                    }
+                    String file_read[]=temp.split("\n");
+                    boolean isFound=false;
+                    for(String record:file_read){
+                        String details[]=record.split(" ");
+                        if(details[0].equalsIgnoreCase(uname_) &&
+
+                                details[1].equalsIgnoreCase(password_)){
+                            isFound=true;
+                            break;
+                        }
+                    }
+                    if(isFound){
+                        Toast.makeText(getBaseContext(),"Login Successful",Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        Toast.makeText(getBaseContext(),"Invalid Credentials",Toast.LENGTH_SHORT).show();
+
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }});
+    }
+}
 
